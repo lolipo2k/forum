ae6b9ce281a8065237d7d0c86b77f6ba1078de3c
lolipo2k <andreasyagin@yandex.ru>
2023-11-14 04:39:46 +0300

    fix btn

--- application/resources/views/presets/default/components/neuroscribe.blade.php
+++ application/resources/views/presets/default/components/neuroscribe.blade.php
@@ -1,6 +1,6 @@
 <div class="neuroscribe-content">
     <h5>AI-ассистент для создания контента</h5>
-    <a href="https://neuroscribe.ru/" class="btn btn-sm btn--base mb-2"> Попробовать бесплатно </a>
+    <a href="https://neuroscribe.ru/" target="__blank" class="btn btn-sm btn--base mb-2"> Попробовать бесплатно </a>
     <div class="neuroscribe-content__info">
         топовые нейросети без VPN
     </div>
