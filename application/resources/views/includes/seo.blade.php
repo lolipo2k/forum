ae992bfddd004ce45b7add73ec85622f384637d3
lolipo2k <lolipo2k@gmail.com>
2023-11-30 09:09:47 +0300

    add seoo to site

--- application/resources/views/includes/seo.blade.php
+++ application/resources/views/includes/seo.blade.php
@@ -20,17 +20,18 @@
 <meta name="apple-mobile-web-app-title" content="{{ $general->siteName($pageTitle) }}">
 {{--<!-- Google / Search Engine Tags -->--}}
 <meta itemprop="name" content="{{ $general->siteName($pageTitle) }}">
-<meta itemprop="description" content="{{ $general->seo_description }}">
+<meta itemprop="description" content="Присоединяйтесь к форуму о нейросетях, где вы сможете обсудить последние новости и разработки в области искусственного интеллекта. Узнайте больше о таких инновационных проектах, как ChatGPT, Midjourney и Stable Diffusion, и поделитесь своими мыслями с экспертами и энтузиастами.">
 <meta itemprop="image" content="{{ getImage(getFilePath('seo') .'/'. $seo->image) }}">
+<meta itemprop="Keywords" content="нейросети, искусственный интеллект, ChatGPT, Midjourney, Stable Diffusion, форум, обсуждение, новости, разработки, эксперты, энтузиасты">
 {{--<!-- Facebook Meta Tags -->--}}
 <meta property="og:type" content="website">
-<meta property="og:title" content="{{ $seo->social_title }}">
-<meta property="og:description" content="{{ $seo->social_description }}">
-<meta property="og:image" content="{{ getImage(getFilePath('seo') .'/'. $seo->image) }}">
-<meta property="og:image:type" content="image/{{ pathinfo(getImage(getFilePath('seo')) .'/'. $seo->image)['extension'] }}">
+<meta property="og:title" content="нейросети, искусственный интеллект, ChatGPT, Midjourney, Stable Diffusion, форум">
+<meta property="og:description" content="Присоединяйтесь к форуму о нейросетях, где вы сможете обсудить последние новости и разработки в области искусственного интеллекта. Узнайте больше о таких инновационных проектах, как ChatGPT, Midjourney и Stable Diffusion, и поделитесь своими мыслями с экспертами и энтузиастами.">
+<meta property="og:image" content="{{ asset('assets/images/general/og-img.jpg') }}">
+<meta property="og:image:type" content="image/jpeg">
 @php $socialImageSize = explode('x', getFileSize('seo')) @endphp
-<meta property="og:image:width" content="{{ $socialImageSize[0] }}">
-<meta property="og:image:height" content="{{ $socialImageSize[1] }}">
+<meta property="og:image:width" content="354">
+<meta property="og:image:height" content="200">
 <meta property="og:url" content="{{ url()->current() }}">
 {{--<!-- Twitter Meta Tags -->--}}
 <meta name="twitter:card" content="summary_large_image">
--- application/resources/views/presets/default/layouts/frontend.blade.php
+++ application/resources/views/presets/default/layouts/frontend.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title> {{ $general->siteName(__($pageTitle)) }}</title>
+    <title>Форум о нейросетях: обсуждение ChatGPT, Midjourney, Stable Diffusion и других нейросетей</title>
     @include('includes.seo')
     <!-- Bootstrap CSS -->
     <link href="{{ asset('assets/common/css/bootstrap.min.css') }}" rel="stylesheet">
@@ -39,7 +39,25 @@
 
     <link rel="stylesheet"
         href="{{ asset($activeTemplateTrue . 'css/color.php') }}?color={{ $general->base_color }}&secondColor={{ $general->secondary_color }}">
-</head>
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(95712239, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/95712239" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+<meta name="yandex-verification" content="2d800ca3dfbe25ab" />
+    </head>
 
 <body>
     @include('presets.default.components.loader')
